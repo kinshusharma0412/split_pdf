f9b54a6eb527a94b882b4f9996f0f404feb58709
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2023-09-26 00:16:58 +0530

    Add files via upload

--- PDF.py
+++ PDF.py
@@ -14,19 +14,23 @@ if on:
 		st.session_state["img"]=[]
 	else:
 		dict=[]
+		
 		for uploaded_file in uploaded_files:
 			name="./"+uploaded_file.name
 			with open(name, "wb") as file:
 				file.write(uploaded_file.getvalue())
 			st.session_state["img"].append((name))
-		
+	
 		pdf_path="./@Polls_Quiz.pdf"
 		file = open(pdf_path, "wb")
 		file.write(img2pdf.convert(st.session_state["img"]))
 		file.close()
+		
+		
 		with open(pdf_path, "rb") as file:
 			if st.download_button(label="Download PDF",data=file,file_name="@Polls_Quiz.pdf",mime="application/octet-stream"):
 				st.session_state["img"]=[]
+				st.stop()
 			
 		
 		
