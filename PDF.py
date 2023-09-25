58bbd4e1f5324409ab04ca6ff1aa3bef55c86a0e
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2023-09-25 23:37:21 +0530

    Add files via upload

--- PDF.py
+++ PDF.py
@@ -10,17 +10,26 @@ if on:
 	
 	st.write('Activate Image to PDF feature')
 	uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-	dict=[]
-	for uploaded_file in uploaded_files:
-		name="./"+uploaded_file.name
-		
-		with open(name, "wb") as file:
-			file.write(uploaded_file.getvalue())
-		image1 = Image.open(name)
-		st.session_state["img"].append(img2pdf.convert(image1.filename))
 	if "img" not in st.session_state:
 		st.session_state["img"]=[]
+		dict=[]
+		for uploaded_file in uploaded_files:
+			name="./"+uploaded_file.name
+			
+			with open(name, "wb") as file:
+				file.write(uploaded_file.getvalue())
+			image1 = Image.open(name)
+			st.session_state["img"].append(img2pdf.convert(image1.filename))
 	else:
+		dict=[]
+		for uploaded_file in uploaded_files:
+			name="./"+uploaded_file.name
+			
+			with open(name, "wb") as file:
+				file.write(uploaded_file.getvalue())
+			image1 = Image.open(name)
+			st.session_state["img"].append(img2pdf.convert(image1.filename))
+		
 		pdf_path="./@Polls_Quiz.pdf"
 		file = open(pdf_path, "wb")
 		for y in st.session_state["img"]:
