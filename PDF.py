215a567ef03ea87626e9a1766c8f7c3d3d5a0c34
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2023-09-25 23:33:39 +0530

    Add files via upload

--- PDF.py
+++ PDF.py
@@ -7,16 +7,7 @@ import glob,img2pdf
 on = st.toggle('Image to PDF feature')
 st.write(st.session_state)
 if on:
-	if "img" not in st.session_state:
-		st.session_state["img"]=[]
-	else:
-		pdf_path="./@Polls_Quiz.pdf"
-		file = open(pdf_path, "wb")
-		for y in st.session_state["img"]:
-			file.write(y)
-		file.close()
-		with open(pdf_path, "rb") as file:
-			btn = st.download_button(label="Download PDF",data=file,file_name="@Polls_Quiz.pdf",mime="application/octet-stream")	
+	
 	st.write('Activate Image to PDF feature')
 	uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 	dict=[]
@@ -27,6 +18,16 @@ if on:
 			file.write(uploaded_file.getvalue())
 		image1 = Image.open(name)
 		st.session_state["img"].append(img2pdf.convert(image1.filename))
+	if "img" not in st.session_state:
+		st.session_state["img"]=[]
+	else:
+		pdf_path="./@Polls_Quiz.pdf"
+		file = open(pdf_path, "wb")
+		for y in st.session_state["img"]:
+			file.write(y)
+		file.close()
+		with open(pdf_path, "rb") as file:
+			btn = st.download_button(label="Download PDF",data=file,file_name="@Polls_Quiz.pdf",mime="application/octet-stream")	
 		
 		
 	
