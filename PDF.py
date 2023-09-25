06274ba0692d1c0c526eb04045dcf1b1cdd843f3
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2023-09-25 23:27:29 +0530

    Add files via upload

--- PDF.py
+++ PDF.py
@@ -5,7 +5,7 @@ from PIL import Image
 import glob,img2pdf
 #from PyPDF2 import PdfReader, PdfWriter, PageObject
 on = st.toggle('Image to PDF feature')
-
+st.write(st.session_state)
 if on:
 	if "img" not in st.session_state:
 		st.session_state["img"]=[]
@@ -16,14 +16,12 @@ if on:
 			file.write(y)
 		file.close()
 		with open(pdf_path, "rb") as file:
-			btn = st.download_button(label="Download PDF",data=file,file_name="@Polls_Quiz.pdf",mime="application/octet-stream")
-		
+			btn = st.download_button(label="Download PDF",data=file,file_name="@Polls_Quiz.pdf",mime="application/octet-stream")	
 	st.write('Activate Image to PDF feature')
 	uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 	dict=[]
 	for uploaded_file in uploaded_files:
 		name="./"+uploaded_file.name
-		st.write(uploaded_file)
 		
 		with open(name, "wb") as file:
 			file.write(uploaded_file.getvalue())
