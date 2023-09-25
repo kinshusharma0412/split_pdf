e0313f6aac2b1303a4b4edd5d3faff7f0ff45812
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2023-09-25 23:22:26 +0530

    Add files via upload

--- PDF.py
+++ PDF.py
@@ -7,14 +7,25 @@ import glob,img2pdf
 on = st.toggle('Image to PDF feature')
 
 if on:
+	pdf_path="./@Polls_Quiz.pdf"
+	file = open(pdf_path, "wb")
+	for y in image_list:
+		file.write(y)
+	file.close()
+	if "img" not in st.session_state:
+		st.session_state["img"]=[]
+	else:
+		pdf_path="./@Polls_Quiz.pdf"
+		file = open(pdf_path, "wb")
+		for y in st.session_state["img"]:
+			file.write(y)
+		file.close()
+		with open(pdf_path, "rb") as file:
+			btn = st.download_button(label="Download PDF",data=file,file_name="@Polls_Quiz.pdf",mime="application/octet-stream")
+		
 	st.write('Activate Image to PDF feature')
 	uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 	dict=[]
-	image_size_x=0
-	image_size_y=[]
-	image_list=[]
-	
-	
 	for uploaded_file in uploaded_files:
 		name="./"+uploaded_file.name
 		st.write(uploaded_file)
@@ -22,16 +33,10 @@ if on:
 		with open(name, "wb") as file:
 			file.write(uploaded_file.getvalue())
 		image1 = Image.open(name)
+		st.session_state["img"].append(img2pdf.convert(image1.filename))
 		
-		image_list.append(img2pdf.convert(image1.filename))
 		
+	
 		
-	pdf_path="./@Polls_Quiz.pdf"
-	file = open(pdf_path, "wb")
-	for y in image_list:
-		file.write(y)
-	file.close()
-		
-	with open(pdf_path, "rb") as file:
-		btn = st.download_button(label="Download PDF",data=file,file_name="@Polls_Quiz.pdf",mime="application/octet-stream")
+	
 		
