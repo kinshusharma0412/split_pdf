7787abfba3b96713e40d768ecd4e0cec22a98cc1
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2023-09-26 07:02:19 +0530

    Add files via upload

--- PDF.py
+++ PDF.py
@@ -15,31 +15,28 @@ if 'dow' not in st.session_state:
 	st.session_state.dow = False
 if on:
 	st.write('Activate Image to PDF feature')
-	uploaded_files = st.file_uploader("Choose a image file (multiple files are accepted)", accept_multiple_files=True)
-	pq=st.empty()
-	if uploaded_files:
-		if len(st.session_state["img"])!=0:
-			file = open(pdf_path, "wb")
-			file.write(img2pdf.convert(st.session_state["img"]))
-			file.close()
-			with open(pdf_path, "rb") as file:
-				if pq.download_button(label="Download PDF",data=file,file_name="@Polls_Quiz.pdf",mime="application/octet-stream"):
-					
-		if st.session_state.clicked:
-			dict=[]
-			for uploaded_file in uploaded_files:
-				name="./"+uploaded_file.name
-				
-				if name not in st.session_state["img"]:
+	with st.form(key="form"):
+		multiple = st.toggle('Do you want to same image comes multiple times in pdf if you upload it multiple times')
+		files = uploaded_files = st.file_uploader("Choose a image file (multiple files are accepted)", accept_multiple_files=True)
+		submit_button = st.form_submit_button(label="Submit your choice")
+	if submit_button:
+		
+		for uploaded_file in uploaded_files:
+				if multiple:
+					name="./"+uploaded_file.name
 					with open(name, "wb") as file:
 						file.write(uploaded_file.getvalue())
 					st.session_state["img"].append((name))
 				else:
-					st.write(name)
-		if st.session_state.clicked:
-			st.session_state.clicked = False
-			file = open(pdf_path, "wb")
-			file.write(img2pdf.convert(st.session_state["img"]))
-			file.close()
-			with open(pdf_path, "rb") as file:
-				pq.download_button(label="Download PDF",data=file,file_name="@Polls_Quiz.pdf",mime="application/octet-stream")
+					name="./"+uploaded_file.name
+					if name not in st.session_state["img"]:
+						with open(name, "wb") as file:
+							file.write(uploaded_file.getvalue())
+						st.session_state["img"].append((name))
+		file = open(pdf_path, "wb")
+		file.write(img2pdf.convert(st.session_state["img"]))
+		file.close()
+		with open(pdf_path, "rb") as file:
+			pq.download_button(label="Download PDF",data=file,file_name="@Polls_Quiz.pdf",mime="application/octet-stream")
+					
+		
